73f2d227251e059e8baa980563d50cfab1ca7c3a
Jade Huynh <35jadeh@gmail.com>
2019-07-02 03:24:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,15 +20,15 @@ function logWhisper(string)
 
 var uppercase = "HELLO!"
 var lowercase = 'hello!'
-var mixedCase = 'Hi there!'
+var mixedcase = 'Hi there!'
 
 function sayHiToGrandma(string)
 {
-  if(string.toLowerCase() === lowercase){
+  if(lowercase.toLowerCase() === lowercase){
     return "I can't hear you!."//"
-  }else if(string.toUpperCase() === uppercase){
+  }else if(uppercase.toUpperCase() === uppercase){
     return 'YES INDEED!'
-  }else if (string === "I love you, Grandma."){
+  }else if (mixedcase === "I love you, Grandma."){
     return "I love you, too."
   }
 }
