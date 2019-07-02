1627e801af6ae8203f37ae0b612eaeb14d6c5995
Jade Huynh <35jadeh@gmail.com>
2019-07-02 03:23:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ var mixedCase = 'Hi there!'
 function sayHiToGrandma(string)
 {
   if(string.toLowerCase() === lowercase){
-    return "I can't hear you!."
+    return "I can't hear you!."//"
   }else if(string.toUpperCase() === uppercase){
-    return "YES INDEED!"
+    return 'YES INDEED!'
   }else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
